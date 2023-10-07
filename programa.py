c937f419a8bd2f83970f89a135cc56ddda0b4907
Joaquin <joaquinmartinezfranco@gmail.com>
2023-10-07 14:59:21 -0300

    lol

--- programa.py
+++ programa.py
@@ -1,7 +1,7 @@
-import numpy
+import numpy as np
 import binascii
 
-def probabilidades_condicionales(matcond,nombre_archivo):
+def probabilidades_condicionales(nombre_archivo):
     bloque=0
 
     with open(nombre_archivo,"rb") as archivo:
@@ -9,22 +9,51 @@ def probabilidades_condicionales(matcond,nombre_archivo):
     
     cadena_bits = ''.join(format(byte, '08b') for byte in contenido_binario)
 
-    print(cadena_bits)
+    #print(cadena_bits)
 
-    matcond = [
-    [0, 0],
-    [0, 0],
-    ]
+    matcond = np.zeros((2, 2), dtype=int)
 
-    matcond=numpy.array(matcond)
+    matcond=np.array(matcond)
     i=1
     for i in range(len(cadena_bits)):
         matcond[int(cadena_bits[i-1]),int(cadena_bits[i])]+= 1
+    return matcond
 
-    print(matcond)
+def matriz_probabilidades_condicionales(matcond):
+    auxmat = np.zeros((2, 2), dtype=float)  # Create a copy to avoid modifying the original matrix
+    
+    aux = matcond[0, 0] + matcond[0, 1]
+    auxmat[0, 0] = matcond[0, 0] / aux
+    auxmat[1, 0] = matcond[1, 0] / aux
+
+    aux = matcond[0, 1] + matcond[1, 1]
+    auxmat[0, 1] = matcond[0, 1] / aux
+    auxmat[1, 1] = matcond[1, 1] / aux
+    return auxmat
+
+
+def fuente_memoria_nula(matprob):
+    return abs(matprob[0,0]-matprob[1,0])<0.05 and abs(matprob[0,1]-matprob[1,1])<0.05
+
+def entropia(matprob):
+    entropia=0
+    for i in range(len(matprob)):
+        for j in range(len(matprob)):
+            if matprob[i,j]!=0:
+                entropia+=matprob[i,j]*np.log2(1/matprob[i,j])
+    return entropia
+
+def probabilidades_extension_orden_n(matcond,orden):
+    mat_prob_orden=np.zeros((2**orden,2**orden),dtype=float)
+    
 
 
 
 filename="tp1_sample0.bin"
-matcond=0
-probabilidades_condicionales(matcond,filename)
+
+matcond=probabilidades_condicionales(filename)
+print(matcond)
+mat_prob_condicionales=matriz_probabilidades_condicionales(matcond)
+if fuente_memoria_nula(mat_prob_condicionales)==True:
+    print("La fuente es de memoria nula")
+print("La entropia es: ",entropia(mat_prob_condicionales))
