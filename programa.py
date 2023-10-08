b3e1e764ead9fe5e63b77858657e0ee8294feffc
Joaquin <joaquinmartinezfranco@gmail.com>
2023-10-08 16:14:26 -0300

    Update programa.py

--- programa.py
+++ programa.py
@@ -1,12 +1,7 @@
 import numpy as np
 import sys
-import copy
 import binascii
-import math
 
-<<<<<<< Updated upstream
-def Genera_Matriz_Acumulada(nombre_archivo):
-=======
 from itertools import product
 
 def calcular_probabilidades_fuente_nula(probabilidades, orden):
@@ -41,7 +36,6 @@ for secuencia, probabilidad in resultados.items():
 
 def probabilidades_condicionales(nombre_archivo):
     bloque=0
->>>>>>> Stashed changes
 
     with open(nombre_archivo,"rb") as archivo: #se abre el archivo en modo de lectura binaria
         contenido_binario=archivo.read() #se lee el contenido del archivo y se guarda  en contenido_binario
@@ -52,17 +46,17 @@ def probabilidades_condicionales(nombre_archivo):
     mat=np.array(mat) #se crea una matriz NumPy int de 2x2 llena de ceros
     
     for i in range(1,len(cadena_bits)): # se recorre secuencialmente la cadena de bits y se acumula en la matriz segun corresponda
-        mat[int(cadena_bits[i]),int(cadena_bits[i+1])]+= 1 #se acumuna en fila de lectura actual y en la columna lectura anterior
+        mat[int(cadena_bits[i-1]),int(cadena_bits[i+1])]+= 1 #se acumuna en fila de lectura actual y en la columna lectura anterior
     return mat
 
 def matriz_probabilidades_condicionales(matcond):
-    auxmat = np.zeros((2, 2), dtype=float)  # se crea una matriz auxiliar de 2x2 para almacenar las probabilidades condicionales
+    auxmat = np.zeros((2, 2), dtype=float)  # Create a copy to avoid modifying the original matrix
     
-    aux = matcond[0, 0] + matcond[0, 1] #se suma la columna 0
+    aux = matcond[0, 0] + matcond[0, 1]
     auxmat[0, 0] = matcond[0, 0] / aux
     auxmat[1, 0] = matcond[1, 0] / aux
 
-    aux = matcond[0, 1] + matcond[1, 1]  #se suma la columna 1
+    aux = matcond[0, 1] + matcond[1, 1]
     auxmat[0, 1] = matcond[0, 1] / aux
     auxmat[1, 1] = matcond[1, 1] / aux
     return auxmat
@@ -106,21 +100,17 @@ else:
 
 filename="tp1_sample0.bin"
 
-mat_acum=Genera_Matriz_Acumulada(filename) 
+mat_acum=Genera_Matriz_Acumulada(filename)
 print(mat_acum)
 mat_prob_condicionales=matriz_probabilidades_condicionales(mat_acum)
 if fuente_memoria_nula(mat_prob_condicionales)==True:
     print("La fuente es de memoria nula")
-<<<<<<< Updated upstream
     print("La entropia es: ",entropia(mat_prob_condicionales))
 else:
     print("La fuente es de memoria no nula")
     vecestacionario= Genera_VecEstacionario(mat_prob_condicionales)
     print("La entropia de la fuente es: ", entropiaNoNula(vecestacionario,mat_prob_condicionales))
     print("El vector estacionario resulta:", vecestacionario)
-
-=======
 print("La entropia es: ",entropia(mat_prob_condicionales))
-vec_probabilidades_simbolos=['0':0.5,'1':0.5]
+dict_probabilidades_simbolos={'0':0,'1':0.5}
 calcular_probabilidades_fuente_nula(mat_prob_condicionales,2)
->>>>>>> Stashed changes
